91670442403e59d5e59b8d084a8b85904de027de
HOME\jiyeo <dlfvkfclfrn@gmail.com>
2024-09-22 01:36:14 +0900

    11일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components"
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <StyledSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </StyledSection>
     </main>
   );
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+  padding: 20px 40px;
+`;
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,40 @@
-export default function Header() {
+import styled from "styled-components";
+
+function Header() {
   return (
-    <header className="header-container">
+    <StyledHeader>
       <h1>OZ코딩스쿨</h1>
-      <ul className="menu-container">
+      <ul>
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 0;
+  flex-wrap: nowrap;
+  padding: 20px 20px;
+  background-color: black;
+
+  ul{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    flex-wrap: nowrap;
+
+    li{
+      font-size: 16px;
+      font-weight: 400;
+      list-style: none;}}
+  
+`
+export default Header;
